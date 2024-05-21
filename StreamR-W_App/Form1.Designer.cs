701970d5187e7faea64087fc7691a0c75ce9ddd4
Mujo Didic <mujocsharpdev@gmail.com>
2024-05-21 06:49:50 +0200

    Add project files.

---
+++ StreamR-W_App/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace StreamR_W_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Speichern = new System.Windows.Forms.Button();
+            this.Laden = new System.Windows.Forms.Button();
+            this.Einfuegen = new System.Windows.Forms.Button();
+            this.EintragTextBox = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // Speichern
+            // 
+            this.Speichern.Location = new System.Drawing.Point(254, 24);
+            this.Speichern.Name = "Speichern";
+            this.Speichern.Size = new System.Drawing.Size(121, 30);
+            this.Speichern.TabIndex = 0;
+            this.Speichern.Text = "Speichern";
+            this.Speichern.UseVisualStyleBackColor = true;
+            this.Speichern.Click += new System.EventHandler(this.Speichern_Click);
+            // 
+            // Laden
+            // 
+            this.Laden.Location = new System.Drawing.Point(254, 73);
+            this.Laden.Name = "Laden";
+            this.Laden.Size = new System.Drawing.Size(121, 30);
+            this.Laden.TabIndex = 1;
+            this.Laden.Text = "Laden";
+            this.Laden.UseVisualStyleBackColor = true;
+            this.Laden.Click += new System.EventHandler(this.Laden_Click);
+            // 
+            // Einfuegen
+            // 
+            this.Einfuegen.Location = new System.Drawing.Point(254, 325);
+            this.Einfuegen.Name = "Einfuegen";
+            this.Einfuegen.Size = new System.Drawing.Size(116, 30);
+            this.Einfuegen.TabIndex = 2;
+            this.Einfuegen.Text = "Einfügen";
+            this.Einfuegen.UseVisualStyleBackColor = true;
+            this.Einfuegen.Click += new System.EventHandler(this.Einfuegen_Click);
+            // 
+            // EintragTextBox
+            // 
+            this.EintragTextBox.Location = new System.Drawing.Point(254, 288);
+            this.EintragTextBox.Name = "EintragTextBox";
+            this.EintragTextBox.Size = new System.Drawing.Size(115, 20);
+            this.EintragTextBox.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 24);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(217, 316);
+            this.listBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 367);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.EintragTextBox);
+            this.Controls.Add(this.Einfuegen);
+            this.Controls.Add(this.Laden);
+            this.Controls.Add(this.Speichern);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Speichern;
+        private System.Windows.Forms.Button Laden;
+        private System.Windows.Forms.Button Einfuegen;
+        private System.Windows.Forms.TextBox EintragTextBox;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ StreamR-W_App/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StreamR_W_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Speichern_Click(object sender, EventArgs e)
+        {
+            //eine neue Instanz von StreamWriter erzeugen
+            StreamWriter textDatei = new StreamWriter("C:\\Test\\daten.txt",
+                true, Encoding.ASCII);
+            //alle daten in der ListBox in die Datei schreiben
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                textDatei.WriteLine(listBox1.Items[i].ToString());
+
+                //die Datei ---StreamWriter--- wieder schließen
+            }
+            textDatei.Close();
+        }
+
+        private void Laden_Click(object sender, EventArgs e)
+        {
+            //existiert die Datei
+            if (File.Exists("C:\\Test\\daten.txt"))
+            {
+                //eine neue instanz von Streamer erzeugen
+                using (StreamReader textDatei = new StreamReader("C:\\daten.txt"))
+                {
+                    //für die Zeilen in der Datei
+                    string zeile;
+                    //die Listbox leeren
+                    listBox1.Items.Clear();
+                    //solange das Ende der Datei nicht erreicht ist,die Daten lesen und in die ListBox schreiben
+                    while (textDatei.EndOfStream == false)
+                    {
+                        zeile = textDatei.ReadLine();
+                        listBox1.Items.Add(zeile);
+                    }
+                }
+            }
+
+        }
+
+        private void Einfuegen_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(EintragTextBox.Text);
+        }
+    }
+}
